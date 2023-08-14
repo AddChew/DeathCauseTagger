5a4905369ecb9d76a0866df82f30a655c754627b
AddChew <addison_chew@outlook.com>
2023-08-14 21:16:04 +0800

    refactor to src folder

--- src/backend/backend/celery.py
+++ src/backend/backend/celery.py
@@ -2,10 +2,10 @@ import os
 from celery import Celery
 
 # Set default Django settings module for celery app
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'icdtagger.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 # Instantiate the celery app
-app = Celery('icdtagger')
+app = Celery('backend')
 
 # Read config from Django settings, the CELERY namespace would make celery config keys have `CELERY`` prefix
 app.config_from_object('django.conf:settings', namespace = 'CELERY')
