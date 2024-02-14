ff1884e84531298dc1892105f8476769da2e2c03
AddChew <addison_chew@outlook.com>
2024-02-15 00:18:47 +0800

    update swagger title and desc

--- backend/backend/api.py
+++ backend/backend/api.py
@@ -1,4 +1,7 @@
 from ninja_extra import NinjaExtraAPI
 
-api = NinjaExtraAPI()
+api = NinjaExtraAPI(
+    title = "Death Cause Tagger",
+    description = "API Documentation for Death Cause Tagger Service.",
+)
 api.auto_discover_controllers()
