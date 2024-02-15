2c3028ac0f24665a1297257f7fc76d988d44b82a
AddChew <addison_chew@outlook.com>
2024-02-16 00:29:06 +0800

    add custom users model

---
+++ backend/tagger/models/users.py
@@ -0,0 +1,92 @@
+import uuid
+
+from django.db import models
+from django.apps import apps
+from django.utils import timezone
+
+from django.contrib.auth.hashers import make_password
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
+
+
+class UserManager(UserManager):
+    """
+    Custom User Manager.
+    """
+    def _create_user(self, username, password, **extra_fields):
+        """
+        Create and save a user with the given username and password.
+        """
+        if not username:
+            raise ValueError("The given username must be set")
+        # Lookup the real model class from the global app registry so this
+        # manager method can be used in migrations. This is fine because
+        # managers are by definition working on the real model.
+        GlobalUserModel = apps.get_model(
+            self.model._meta.app_label, self.model._meta.object_name
+        )
+        username = GlobalUserModel.normalize_username(username)
+        user = self.model(username=username, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(username, password, **extra_fields)
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(username, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom User Model.
+    """
+    username_validator = UnicodeUsernameValidator()
+
+    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
+    username = models.CharField(
+        _("username"),
+        max_length=150,
+        unique=True,
+        help_text=_(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        validators=[username_validator],
+        error_messages={
+            "unique": _("A user with that username already exists."),
+        },
+    )
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site."),
+    )
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "username"
+
+    class Meta:
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
