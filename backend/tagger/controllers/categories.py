cfbe7b9216ca1ec05a7906502e82a1e72c3c2e07
AddChew <addison_chew@outlook.com>
2024-02-15 22:27:18 +0800

    set tags to lowercase and add async

--- backend/tagger/controllers/categories.py
+++ backend/tagger/controllers/categories.py
@@ -4,13 +4,13 @@ from ninja_extra import api_controller, http_get
 
 @api_controller(
     prefix_or_class = "/categories",
-    tags = ["Categories"],
+    tags = ["categories"],
     permissions = [],
 )
 class CategoryController:
 
     @http_get()
-    def read_categories(self) -> List[Dict]: # TODO:
+    async def read_categories(self) -> List[Dict]: # TODO:
         """Read categories"""
         return [
             {'code': 'A00', 'description': 'testing'}
