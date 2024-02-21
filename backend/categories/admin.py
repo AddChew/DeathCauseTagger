3e37f1999ccacd7fb44942668673644162eff410
AddChew <addison_chew@outlook.com>
2024-02-21 23:17:47 +0800

    refactor admin

--- backend/categories/admin.py
+++ backend/categories/admin.py
@@ -1,37 +1,12 @@
 from django.contrib import admin
 
+from base.admin import BaseAdmin
 from categories.models import Category
 
 
 @admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(BaseAdmin):
     """
     Admin Class for Category Model.
     """
-    list_display = (
-        "description", "get_status_display", "created_by", "created_on", "updated_by", 
-        "updated_on", "status_updated_by", "status_updated_on",
-    )
-    list_filter = ("status",)
-    search_fields = ("description__icontains",)
-
-    def get_status_display(self, obj):
-        return obj.get_status_display()
-    
-    get_status_display.short_description = "status"
-
-    def save_model(self, request, obj, form, change):
-        user = request.user
-
-        if not change:
-            obj.created_by = user
-
-        if obj.fields_tracker.has_changed("status"):
-            obj.status_updated_by = user
-        
-        obj.updated_by = user
-
-        super().save_model(request, obj, form, change)
-
-# TODO: message when status change
-# TODO: bulk change status action + message
+    pass
