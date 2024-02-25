f5cb3ef26872c365b2bc3da05096be788edb984f
AddChew <addison_chew@outlook.com>
2024-02-25 20:42:22 +0800

    add codes model

--- backend/codes/admin.py
+++ backend/codes/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.utils import timezone
 
-# Register your models here.
+from base.admin import BaseAdmin
+from categories.models import Category
+
+
+@admin.register(Category)
+class CodeAdmin(BaseAdmin):
+    """
+    Admin Class for Code Model.
+    """
+    actions = ["mark_active", "mark_inactive"]
+
+    @admin.action(description = "Mark selected Codes as active")
+    def mark_active(self, request, queryset):
+        """
+        Mark selected codes as active.
+        """
+        self.action(
+            request, queryset, action = "active", 
+            update_fields = {
+                "is_active": True,
+                "is_active_updated_by": request.user.id,
+                "is_active_updated_on": timezone.now(),
+        })
+
+    @admin.action(description = "Mark selected Codes as inactive")
+    def mark_inactive(self, request, queryset):
+        """
+        Mark selected codes as inactive.
+        """
+        self.action(
+            request, queryset, action = "inactive", 
+            update_fields = {
+                "is_active": False,
+                "is_active_updated_by": request.user.id,
+                "is_active_updated_on": timezone.now(),
+        })
--- backend/codes/models.py
+++ backend/codes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from model_utils import FieldTracker
 
-# Create your models here.
+from base.models import BaseModel, BaseIsActiveModel
+
+
+class Code(BaseModel, BaseIsActiveModel):
+    """
+    Code Model.
+    """    
+    description = models.CharField(max_length = 200, unique = True)    
+    is_active_tracker = FieldTracker(fields = ["is_active"])
+
+    class Meta:
+        verbose_name_plural = "Codes"
+
+    def __str__(self):
+        return self.description
