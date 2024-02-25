4c08c5b261ab70b526ef4fa03ad28f086e236578
AddChew <addison_chew@outlook.com>
2024-02-25 22:32:04 +0800

    add periods model

--- backend/periods/admin.py
+++ backend/periods/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
+from django.utils import timezone
 
-# Register your models here.
+from base.admin import BaseAdmin
+from periods.models import Period
+
+
+@admin.register(Period)
+class PeriodAdmin(BaseAdmin):
+    """
+    Admin Class for Period Model.
+    """
+    list_display = (
+        "code_input",
+        "threshold",
+        "code_below",
+        "code_equal",
+        "code_above"
+        "created_by", "created_on", 
+        "updated_by", "updated_on", 
+        "is_active", "is_active_updated_by", "is_active_updated_on",
+    )
+    search_fields = (
+        "code_input__description__icontains", 
+        "code_below__description__icontains", 
+        "code_equal__description__icontains",  
+        "code_above__description__icontains", 
+        "threshold"
+    )
+    ordering = ("code_input",)
+    actions = ["mark_active", "mark_inactive"]
+
+    @admin.action(description = "Mark selected Periods as active")
+    def mark_active(self, request, queryset):
+        """
+        Mark selected periods as active.
+        """
+        self.action(
+            request, queryset, action = "active", 
+            update_fields = {
+                "is_active": True,
+                "is_active_updated_by": request.user.id,
+                "is_active_updated_on": timezone.now(),
+        })
+
+    @admin.action(description = "Mark selected Periods as inactive")
+    def mark_inactive(self, request, queryset):
+        """
+        Mark selected periods as inactive.
+        """
+        self.action(
+            request, queryset, action = "inactive", 
+            update_fields = {
+                "is_active": False,
+                "is_active_updated_by": request.user.id,
+                "is_active_updated_on": timezone.now(),
+        })
--- backend/periods/models.py
+++ backend/periods/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from model_utils import FieldTracker
 
-# Create your models here.
+from base.models import BaseModel, BaseIsActiveModel
+from codes.models import Code
+
+
+class Period(BaseModel, BaseIsActiveModel):
+    """
+    Period Model.
+    """
+    code_input = models.OneToOneField(Code, on_delete = models.CASCADE, related_name = "period")
+    threshold = models.PositiveSmallIntegerField()
+
+    code_below = models.ForeignKey(Code, on_delete = models.CASCADE, related_name = "below")
+    code_equal = models.ForeignKey(Code, on_delete = models.CASCADE, related_name = "equal")
+    code_above = models.ForeignKey(Code, on_delete = models.CASCADE, related_name = "above")
+
+    is_active_tracker = FieldTracker(fields = ["is_active"])
+
+    class Meta:
+        verbose_name_plural = "Periods"
