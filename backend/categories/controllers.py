c9f7d08c6a7bde3bd4eecdabb0419a5fe2f04cba
AddChew <addison_chew@outlook.com>
2024-10-16 00:28:40 +0800

    add searching

--- backend/categories/controllers.py
+++ backend/categories/controllers.py
@@ -1,7 +1,8 @@
 from ninja_extra.ordering import ordering
 from ninja_extra.pagination import paginate
-from ninja_extra import api_controller, route
+from ninja_extra.searching import searching
 
+from ninja_extra import api_controller, route
 from ninja_jwt.authentication import AsyncJWTTokenUserAuth
 from ninja_extra.schemas import NinjaPaginationResponseSchema
 
@@ -22,6 +23,7 @@ class CategoryController:
     @route.get()
     @paginate
     @ordering
+    @searching(search_fields = ["@description", "=is_active"])
     async def read_categories(self) -> NinjaPaginationResponseSchema[CategorySchema]:
         """
         Read categories.
