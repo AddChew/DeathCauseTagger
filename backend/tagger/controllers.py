2dc987dd64f9060d72f1cab4048ac045e8395c78
AddChew <addison_chew@outlook.com>
2024-10-20 23:15:17 +0800

    refactor

--- backend/tagger/controllers.py
+++ backend/tagger/controllers.py
@@ -6,6 +6,7 @@ from django.forms.models import model_to_dict
 from ninja_extra import api_controller, route
 from ninja_jwt.authentication import AsyncJWTTokenUserAuth
 
+from periods.models import Period
 from mappings.models import Mapping
 from tagger.schemas import TagSchema
 
@@ -25,30 +26,28 @@ class TaggerController:
         Tag single death cause.
         """
         try:
-            get_mapping_async = sync_to_async(Mapping.objects.get)
-            mapping = await get_mapping_async(
+            mapping = await Mapping.objects.select_related("code").aget(
                 death_cause__description__iexact = death_cause, 
                 is_active = True, 
                 is_open = False
-            )
+            ) # TODO: period
             if not mapping.is_option:
-                mapping = await get_mapping_async(
-                    code = mapping.code_id,
+                mapping = await mapping.code.mappings.aget(
                     is_option = True, 
                     is_active = True, 
                     is_open = False
                 )
-            tag = await sync_to_async(self.model_to_dict)(mapping, fields = ["code", "death_cause"]) # TODO: add period logic
             return {
                 "description": death_cause,
                 "period": period,
-                "tag": tag
+                "tag": await self.model_to_dict(mapping, fields = ["code", "death_cause"])
             }
 
         except Mapping.DoesNotExist:
             pass # TODO: retrieve options
         
     @staticmethod
+    @sync_to_async
     def model_to_dict(instance: models.Model, fields: list = None, exclude: list = None) -> dict:
         """
         Convert model to dict.
